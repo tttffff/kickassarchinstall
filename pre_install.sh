3578d4bfb5a8045d149456ce4266afc5ba2840a5
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-14 20:55:29 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -9,5 +9,5 @@ python -m archinstall --config archinstall_config.txt --disk_layouts archinstall
 
 mount -o subvol=@/.snapshots/0/snapshot /dev/sda2 /mnt
 sed -i 's:subvolid=[0-9]\+,subvol=/@/.snapshots/0/snapshot::' /mnt/etc/fstab
-cp post_install.sh /mnt
+cp ../post_install.sh /mnt
 umount /mnt
