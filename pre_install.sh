957258fc244304545dd195cb353ae9eae596efd8
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-14 21:03:33 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-git clone https://github.com/archlinux/archinstall
-cp *txt archinstall
-cd archinstall
-python setup.py install
-python -m archinstall --config archinstall_config.txt --disk_layouts archinstall_disk_layout.txt
-
-mount -o subvol=@/.snapshots/0/snapshot /dev/sda2 /mnt
-sed -i 's:subvolid=[0-9]\+,subvol=/@/.snapshots/0/snapshot::' /mnt/etc/fstab
-cp ../post_install.sh /mnt
+git clone https://github.com/archlinux/archinstall && \
+cp *txt archinstall && \
+cd archinstall && \
+python setup.py install && \
+python -m archinstall --config archinstall_config.txt --disk_layouts archinstall_disk_layout.txt && \
+mount -o subvol=@/.snapshots/0/snapshot /dev/sda2 /mnt && \
+sed -i 's:subvolid=[0-9]\+,subvol=/@/.snapshots/0/snapshot::' /mnt/etc/fstab && \
+cp ../post_install.sh /mnt && \
 umount /mnt
