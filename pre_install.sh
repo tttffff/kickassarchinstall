ad158c1b29efed5ad198cf9a036fbf5aac61fd2a
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-14 22:20:06 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -6,6 +6,6 @@ python setup.py install &&
 python -m archinstall --config archinstall_config.txt --disk_layouts archinstall_disk_layout.txt &&
 mount -o subvol=@/.snapshots/0/snapshot /dev/sda2 /mnt &&
 # I don't know why archinstall adds this into fstab, it doesn't seem to do anthing, and we deffo don't want it later
-sed -i 's:subvolid=[0-9]\+,subvol=/@/.snapshots/0/snapshot::' /mnt/etc/fstab &&
+sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/0/snapshot::' /mnt/etc/fstab &&
 cp ../post_install.sh /mnt &&
 umount /mnt
