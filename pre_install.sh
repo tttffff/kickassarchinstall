b733644ecfbfcb898069e84919c066b5a02c863a
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-15 21:15:17 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -6,7 +6,7 @@ python setup.py install &&
 sleep 2 &&
 python -m archinstall --config archinstall_config.txt --disk_layouts archinstall_disk_layout.txt &&
 mount -o subvol=@/.snapshots/1/snapshot /dev/sda2 /mnt &&
-# I don't know why archinstall adds this into fstab, it doesn't seem to do anthing, and we deffo don't want it later
+# I don't know why genfstab adds this into fstab, it doesn't seem to do anthing, and we deffo don't want it later
 sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/1/snapshot::' /mnt/etc/fstab &&
 cp ../post_install.sh /mnt &&
 umount /mnt
