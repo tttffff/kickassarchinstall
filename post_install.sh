6d69063160cbf500e33d92768e6443d1bf0a32ba
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-17 20:18:41 +0000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,13 @@
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="video=1920x1080 /' /etc/default/grub &&
 sed -i 's/rootflags=subvol=${rootsubvol}//' /etc/grub.d/10_linux &&
 
-# Tell Pacman not to install cron files
+# Turn fsck off (we don't want this for btrfs)
+sed 's/ fsck//' /etc/mkinitcpio.conf &&
+mkinitcpio -P &&
+
+# Tell Pacman not to install cron files for snapper (we will use systemd)
+sed -E -i 's:#(NoUpgrade.*):\1 etc/cron.daily/snapper etc/cron.hourly/snapper:' /etc/pacman.conf
+sed -E -i 's:#(NoExtract.*):\1 etc/cron.daily/snapper etc/cron.hourly/snapper:' /etc/pacman.conf
 
 # Create snapper configs, a bit of a jig, but it works. Also change TIMELINE options to something reasonable.
 pacman -S snapper &&
@@ -21,13 +27,14 @@ sed -i 's/<description>After Snapper Installed/<description>First Root/' /.snaps
 pacman -S grub-btrfs snap-pac &&
 btrfs subvolume set-default / &&
 systemctl enable --now grub-btrfs.path &&
-
+# Re-create grub config
 grub-mkconfig -o /boot/grub/grub.cfg &&
+
 # Don't want updatedb to include files from our snapshots
 sed -i 's/PRUNENAMES = "/PRUNENAMES = ".snapshots /' /etc/updatedb.conf &&
-# snapper -c root create --description "Before LARBS" &&
 systemctl enable --now snapper-timeline.timer &&
 systemctl enable --now snapper-cleanup.timer &&
+# snapper -c root create --description "Before LARBS" &&
 # curl -LO larbs.xyz/larbs.sh &&
 # sh larbs.sh &&
 snapper -c root create --description "After Kickassarchinstall"
