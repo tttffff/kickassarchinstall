548d583a2d12726430cc7c8ff162e0a47158aded
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-17 22:04:01 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -18,8 +18,15 @@ read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][e
 sed -i "s:HARDDRIVE_GOES_HERE:${drive}:" archinstall_disk_layout.txt &&
 sed -i "s:HARDDRIVE_GOES_HERE:${drive}:" archinstall_config.txt &&
 
-echo "About to run ArchInstall with our config. You will be able to pick things like your user accounts, profile etc" &&
-echo "I suggest picking profile xorg so we can get the drivers set up ready for LARBS without installing any bloat" &&
+printf "
+About to run ArchInstall with our config. You will be able to pick things like your user accounts, profile etc
+I suggest picking profile xorg so we can get the drivers set up ready for LARBS without installing any bloat
+
+Unless you are SURE, select NO to: Would you like to chroot into the newly created installation and perform post-installation configuration?
+The scripts will do all the post configuration that you need.
+
+" &&
+
 read -p "Press enter to continue..." &&
 
 # Get the latest ArchInstall (it will work with this set up), copy config files, install and run
@@ -34,16 +41,14 @@ cd .. &&
 # Change /etc/fstab before booting into new system
 mount -o subvol=@/.snapshots/1/snapshot /dev/sda2 /mnt &&
 # I don't know why genfstab adds this into fstab, it doesn't seem to do anthing, and we deffo don't want it later
-sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/1/snapshot::' /mnt/etc/fstab &&
+sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/1/snapshot::' /mnt/archinstall/etc/fstab &&
 # Better options for our BTFS volumes
-sed -i 's/rw,relatime,space_cache=v2/defaults,noatime,autodefrag,ssd,discard=async,compress=zstd,space_cache=v2/' /mnt/etc/fstab &&
-sed -E -i 's/((images|mariadb|mysql|pgsql)\s+btrfs\s+defaults,)/\1nodatacow,/' /mnt/etc/fstab &&
+sed -i 's/rw,relatime,space_cache=v2/defaults,noatime,autodefrag,ssd,discard=async,compress=zstd,space_cache=v2/' /mnt/archinstall/etc/fstab &&
+sed -E -i 's/((images|mariadb|mysql|pgsql)\s+btrfs\s+defaults,)/\1nodatacow,/' /mnt/archinstall/etc/fstab &&
 # Copy packman hook to backup /boot when there's any majot changes
-cp 50-bootbackup.hook /mnt/etc/pacman.d/hooks/50-bootbackup.hook &&
-umount /mnt &&
+cp 50-bootbackup.hook /mnt/archinstall/etc/pacman.d/hooks/50-bootbackup.hook &&
 
 # Copy post install script to root user
-mount -o subvol=@/root /dev/sda2 /mnt &&
-cp ../post_install.sh /mnt &&
+cp ../post_install.sh /mnt/archinstall/mnt &&
 
 echo "SMASHED IT. Please reboot and run the post_install script (it is in /root)"
