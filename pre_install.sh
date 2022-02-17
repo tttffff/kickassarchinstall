7701c076a64c375d36df9e98d094687e2a9edfd2
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-17 20:37:56 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-clear
-echo "Welcome to the kickassarchinstall. Setting up with BTRFS in a more OpenSUSE style (snapper rollbacks work)"
-lsblk | grep disk
-read -p "Enter the drive that you would like to install on (e.g. sda): " drive
-drive=/dev/$drive
-echo "This script will completly wipe ${drive}"
+clear &&
+echo "Welcome to the kickassarchinstall. Setting up with BTRFS in a more OpenSUSE style (snapper rollbacks work)" &&
+lsblk | grep disk &&
+read -p "Enter the drive that you would like to install on (e.g. sda): " drive &&
+drive=/dev/$drive &&
+echo "This script will completly wipe ${drive}" &&
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-sed -i "s:HARDDRIVE_GOES_HERE:${drive}:" archinstall_disk_layout.txt
-sed -i "s:HARDDRIVE_GOES_HERE:${drive}:" archinstall_config.txt
+sed -i "s:HARDDRIVE_GOES_HERE:${drive}:" archinstall_disk_layout.txt &&
+sed -i "s:HARDDRIVE_GOES_HERE:${drive}:" archinstall_config.txt &&
 
-echo "About to run ArchInstall with our config. You will be able to pick things like your user accounts, profile etc"
-echo "I suggest picking profile xorg so we can get the drivers set up ready for LARBS without installing any bloat"
-read -p "Press enter to continue..."
+echo "About to run ArchInstall with our config. You will be able to pick things like your user accounts, profile etc" &&
+echo "I suggest picking profile xorg so we can get the drivers set up ready for LARBS without installing any bloat" &&
+read -p "Press enter to continue..." &&
 
 # Get the latest ArchInstall (it will work with this set up), copy config files, install and run
 git clone https://github.com/archlinux/archinstall &&
 cp *txt archinstall &&
 cd archinstall &&
 python setup.py install &&
-sleep 2 &&
+echo "Waiting 5 seconds..." &&
+sleep 5 &&
 python -m archinstall --config archinstall_config.txt --disk_layouts archinstall_disk_layout.txt &&
-cd ..
+cd .. &&
 
 # Change /etc/fstab before booting into new system
 mount -o subvol=@/.snapshots/1/snapshot /dev/sda2 /mnt &&
@@ -31,7 +32,7 @@ sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/1/snapshot::' /mnt/etc/fstab &&
 sed -i 's/rw,relatime,space_cache=v2/defaults,noatime,autodefrag,ssd,discard=async,compress=zstd,space_cache=v2/' /mnt/etc/fstab &&
 sed -E -i 's/((images|mariadb|mysql|pgsql)\s+btrfs\s+defaults,)/\1nodatacow,/' /mnt/etc/fstab &&
 # Copy packman hook to backup /boot when there's any majot changes
-cp 50-bootbackup.hook /mnt/etc/pacman.d/hooks/50-bootbackup.hook
+cp 50-bootbackup.hook /mnt/etc/pacman.d/hooks/50-bootbackup.hook &&
 umount /mnt &&
 
 # Copy post install script to root user
