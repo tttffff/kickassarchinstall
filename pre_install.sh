dc14ed9cd8be3de1bcbde3fc3a042ecf2863a751
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-17 20:01:47 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -21,6 +21,7 @@ cd archinstall &&
 python setup.py install &&
 sleep 2 &&
 python -m archinstall --config archinstall_config.txt --disk_layouts archinstall_disk_layout.txt &&
+cd ..
 
 # Change /etc/fstab before booting into new system
 mount -o subvol=@/.snapshots/1/snapshot /dev/sda2 /mnt &&
@@ -29,6 +30,8 @@ sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/1/snapshot::' /mnt/etc/fstab &&
 # Better options for our BTFS volumes
 sed -i 's/rw,relatime,space_cache=v2/defaults,noatime,autodefrag,ssd,discard=async,compress=zstd,space_cache=v2/' /mnt/etc/fstab &&
 sed -E -i 's/((images|mariadb|mysql|pgsql)\s+btrfs\s+defaults,)/\1nodatacow,/' /mnt/etc/fstab &&
+# Copy packman hook to backup /boot when there's any majot changes
+cp 50-bootbackup.hook /mnt/etc/pacman.d/hooks/50-bootbackup.hook
 umount /mnt &&
 
 # Copy post install script to root user
