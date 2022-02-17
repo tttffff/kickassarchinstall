71814158a7a0900af7e300214da2e74ae850fe9b
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-17 18:58:47 +0000

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -13,9 +13,9 @@ mount -o subvol=@/.snapshots/1/snapshot /dev/sda2 /mnt &&
 # I don't know why genfstab adds this into fstab, it doesn't seem to do anthing, and we deffo don't want it later
 sed -i 's:,subvolid=[0-9]\+,subvol=/@/.snapshots/1/snapshot::' /mnt/etc/fstab &&
 # Better options for our BTFS volumes
-sed -i 's/rw,relatime,space_cache=v2/defaults,noatime,autodefrag,ssd,discard=async,compress=zstd,space_cache=v2/' /mnt/etc/fstab
-sed -E -i 's/((images|mariadb|mysql|pgsql)\s+btrfs\s+defaults,)/\1nodatacow,/' /mnt/etc/fstab
-umount /mnt
+sed -i 's/rw,relatime,space_cache=v2/defaults,noatime,autodefrag,ssd,discard=async,compress=zstd,space_cache=v2/' /mnt/etc/fstab &&
+sed -E -i 's/((images|mariadb|mysql|pgsql)\s+btrfs\s+defaults,)/\1nodatacow,/' /mnt/etc/fstab &&
+umount /mnt &&
 
 # Copy post install script to root user
 mount -o subvol=@/root /dev/sda2 /mnt &&
