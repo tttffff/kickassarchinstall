dfe148a0c30fa488b207eeebbc35b0f3f049da6c
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-17 19:48:25 +0000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Better resolution when booting, and don't set subvolume for / (this way we will boot from the default BTRFS subvolume)
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="video=1920x1080 /' /etc/default/grub &&
 sed -i 's/rootflags=subvol=${rootsubvol}//' /etc/grub.d/10_linux &&
+
 # Tell Pacman not to install cron files
+
+# Create snapper configs, a bit of a jig, but it works. Also change TIMELINE options to something reasonable.
 pacman -S snapper &&
 snapper -c home create-config /home &&
 # Change the config for TIMELINE
@@ -18,10 +23,11 @@ btrfs subvolume set-default / &&
 systemctl enable --now grub-btrfs.path &&
 
 grub-mkconfig -o /boot/grub/grub.cfg &&
+# Don't want updatedb to include files from our snapshots
 sed -i 's/PRUNENAMES = "/PRUNENAMES = ".snapshots /' /etc/updatedb.conf &&
 # snapper -c root create --description "Before LARBS" &&
 systemctl enable --now snapper-timeline.timer &&
 systemctl enable --now snapper-cleanup.timer &&
 # curl -LO larbs.xyz/larbs.sh &&
 # sh larbs.sh &&
-snapper -c root create --description "After Install"
+snapper -c root create --description "After Kickassarchinstall"
