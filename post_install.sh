f01eea537a535adc6eccf2b16626b7d73384bbcb
tttffff <96426677+tttffff@users.noreply.github.com>
2022-02-18 12:18:20 +0000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# For users who pick this option (further down)
+function install_larbs {
+  snapper -c root create --description "Before LARBS" &&
+  curl -LO larbs.xyz/larbs.sh &&
+  sh larbs.sh
+}
+
 # Better resolution when booting, and don't set subvolume for / (this way we will boot from the default BTRFS subvolume)
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="video=1920x1080 /' /etc/default/grub &&
 sed -i 's/rootflags=subvol=${rootsubvol}//' /etc/grub.d/10_linux &&
@@ -28,15 +35,19 @@ snapper -c root create --description "After Snapper Installed" &&
 cp /.snapshots/2/info.xml /.snapshots/1/info.xml &&
 sed -i 's/<num>2/<num>1/' /.snapshots/1/info.xml &&
 sed -i 's/<description>After Snapper Installed/<description>First Root/' /.snapshots/1/info.xml &&
+
+echo "Would you like to install LARBS? (https://larbs.xyz)" &&
+echo "Personally, I think it's great, but you may not want to do this if you are using a DM like Gnome or KDE" &&
+read -p "(Y/N): " confirm && [[ $confirm == [yY] ]] && install_larbs()
+
+# So that we get a snapshots added to grub and a snapshot after every pacman command
 pacman --noconfirm -S grub-btrfs snap-pac &&
-btrfs subvolume set-default / &&
 systemctl enable --now grub-btrfs.path &&
 # Re-create grub config
+btrfs subvolume set-default / &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
 
 systemctl enable --now snapper-timeline.timer &&
 systemctl enable --now snapper-cleanup.timer &&
-snapper -c root create --description "Before LARBS" &&
-curl -LO larbs.xyz/larbs.sh &&
-sh larbs.sh &&
+
 snapper -c root create --description "After Kickassarchinstall"
